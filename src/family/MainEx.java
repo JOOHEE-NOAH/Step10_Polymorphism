fb90ee6ccacd67358d0db8df870fe8401a864e4a
JOOHEE-NOAH <joohee195@gmail.com>
2022-02-09 22:15:21 +0900

    initial commit

---
+++ src/family/MainEx.java
@@ -0,0 +1,30 @@
+package family;
+
+public class MainEx extends Family implements Job{
+	public MainEx(String name) {
+		super(name);
+	
+	}
+	@Override
+	public String work() {
+	return super.toString();
+	}
+
+	public static void main(String[] args) {
+		String com=args[0];
+	    Job fm=null; 
+	
+		if(com.equals("father")|| com.equals("FATHER")){
+		 fm=new Father(); 
+		
+		}else if(com.equals("mother")|| com.equals("MOTHER")) {
+		fm=new Mother();
+		
+		}else if(com.equals("son")|| com.equals("SON")) {
+			fm=new Son();
+		}
+		fm.work();
+		
+	}
+
+}
