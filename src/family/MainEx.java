7e3e069954d09da049745ab3a30d19c078d78a94
JOOHEE-NOAH <joohee195@gmail.com>
2022-02-10 09:34:52 +0900

    Family 배열 생성, for문으로 패밀리 객체 생성 후 출력

--- src/family/MainEx.java
+++ src/family/MainEx.java
@@ -1,30 +1,43 @@
 package family;
 
-public class MainEx extends Family implements Job{
-	public MainEx(String name) {
-		super(name);
-	
-	}
-	@Override
-	public String work() {
-	return super.toString();
-	}
+public class MainEx {
 
 	public static void main(String[] args) {
-		String com=args[0];
-	    Job fm=null; 
-	
-		if(com.equals("father")|| com.equals("FATHER")){
-		 fm=new Father(); 
+//		Father fa=new Father("홍길동");
+//		System.out.println(fa.toString());
 		
-		}else if(com.equals("mother")|| com.equals("MOTHER")) {
-		fm=new Mother();
+	//	Family fa=new Father("홍길동");
+	//	System.out.println(fa.toString()); //이걸 아래처럼 배열로 바꾸자
 		
-		}else if(com.equals("son")|| com.equals("SON")) {
-			fm=new Son();
-		}
-		fm.work();
+		Family [] family= new Family[]{
+			new Father("홍길동"),//배열에 참조 자료형 대입
+			new Mother("패티"),
+			new Son("뽀로로")
+		}; //배열이니 세미콜론
 		
-	}
+		for(Family ob:family)//패밀리 객체 생성
+			System.out.println(ob.toString());//System.out.println(ob); 동일한 표현
+		
+		
+		
+		
+		
+		
+//-------------내가한거---------------		
+//		String com=args[0];
+//	    Job fm=null; 
+//	
+//		if(com.equals("father")|| com.equals("FATHER")){
+//		 fm=new Father(); 
+//		
+//		}else if(com.equals("mother")|| com.equals("MOTHER")) {
+//		fm=new Mother();
+//		
+//		}else if(com.equals("son")|| com.equals("SON")) {
+//			fm=new Son();
+//		}
+//		fm.work();
+//		
+	}
 
 }
