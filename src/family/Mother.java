3573718e4d55d8eb0bb82732bc836a8dfa331714
JOOHEE-NOAH <joohee195@gmail.com>
2022-02-10 09:33:06 +0900

    toString에+work()추가

--- src/family/Mother.java
+++ src/family/Mother.java
@@ -1,15 +1,19 @@
 package family;
 
 public class Mother extends Family implements Job{
-	public Mother() {}
-	public Mother(String name) {
-		super(name);
+	public Mother() {
+		
 	}
+	public Mother(String name) {//이미 부모클래스가 가지고있는 함수이니 오버라이드
+	super(name);
+	}
+	
 	@Override
-	public String work() {
+	public String work() {//인터페이스에서 가져올 내용이라 오버라이드
 		return "엄마는 집안일을한다";
 	}
-	public String toString() {
-		return super.toString();
+	@Override
+	public String toString() {//이미 부모클래스가 가지고있는 함수이니 오버라이드
+		return super.toString()+work()+"\n";
 	}
 }
